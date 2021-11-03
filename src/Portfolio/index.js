622581b0f87846fa6843331c4f647f8ef34be2f0
Amit Kumar Soni <amitsoniforbhu@gmail.com>
2021-11-03 10:14:55 +0530

    tailwind

--- src/Portfolio/index.js
+++ src/Portfolio/index.js
@@ -1,4 +1,4 @@
-import React , {Suspense }from "react";
+import React, { Suspense } from "react";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 import {
@@ -8,25 +8,17 @@ import {
   useLocation,
   useRouteMatch,
 } from "react-router-dom";
-
+import TopNav from './Topnav'
 import Sidenav from "./Sidenav/Sidenav";
-const  About = React.lazy(() => import('./About/About.js'));
+const About = React.lazy(() => import('./About/About.js'));
 //const  Sidenav = React.lazy(() => import("./Sidenav/Sidenav"));
-const  Experience = React.lazy(() => import("./Experience/Experience"));
-const  Education = React.lazy(() => import("./Education/Education"));
-const  Skills = React.lazy(() => import("./Skills/Skills"));
-const  Interests = React.lazy(() => import("./Interests/Interests"));
-const  Award = React.lazy(() => import("./Award/Award"));
-const  NotFound = React.lazy(() => import("./404"));
+const Experience = React.lazy(() => import("./Experience/Experience"));
+const Education = React.lazy(() => import("./Education/Education"));
+const Skills = React.lazy(() => import("./Skills/Skills"));
+const Interests = React.lazy(() => import("./Interests/Interests"));
+const Award = React.lazy(() => import("./Award/Award"));
+const NotFound = React.lazy(() => import("./404"));
 
-
-
-// import Experience from "./Experience/Experience";
-// import Education from "./Education/Education";
-// import Skills from "./Skills/Skills";
-// import Interests from "./Interests/Interests";
-// import Award from "./Award/Award";
-// import NotFound from "./404";
 const Portfolio = () => {
   let { path, url } = useRouteMatch();
   let location = useLocation();
@@ -34,44 +26,47 @@ const Portfolio = () => {
   console.log("path, url>>>>", path, url);
 
   return (
-    <div className="portfolioContainer">
-      <Sidenav />
-      
+    <div className="mportfolioContainer">
+      <TopNav />
+
       <TransitionGroup>
         <CSSTransition key={location.key} classNames="fade" timeout={300}>
 
-        <Suspense fallback={<div>Loading...</div>}>
-        <Switch>
-            <Route path={`${path}/about`} exact>
-              <About />
-            </Route>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+              <Route path={`${path}/`} exact>
+                <About />
+              </Route>
+              <Route path={`${path}/about`} exact>
+                <About />
+              </Route>
 
-            <Route path={`${path}/experiance`} exact>
-              <Experience />
-            </Route>
+              <Route path={`${path}/experiance`} exact>
+                <Experience />
+              </Route>
 
-            <Route path={`${path}/education`}>
-              <Education />
-            </Route>
+              <Route path={`${path}/education`}>
+                <Education />
+              </Route>
 
-            <Route path={`${path}/skills`}>
-              <Skills />
-            </Route>
+              <Route path={`${path}/skills`}>
+                <Skills />
+              </Route>
 
-            <Route path={`${path}/interest`}>
-              <Interests />
-            </Route>
+              <Route path={`${path}/interest`}>
+                <Interests />
+              </Route>
 
-            <Route path={`${path}/award`}>
-              <Award />
-            </Route>
+              <Route path={`${path}/award`}>
+                <Award />
+              </Route>
 
-            <Route path="*">
-              <NotFound />
-            </Route>
-          </Switch>
-        </Suspense>
-          
+              <Route path="*">
+                <NotFound />
+              </Route>
+            </Switch>
+          </Suspense>
+
         </CSSTransition>
       </TransitionGroup>
     </div>
